4f716be57093d5cb43b32215b6f843c035186e6e
Senju, Hikari <h@senju.me>
2014-04-23 21:56:27 -0400

    first commit

---
+++ Assassination/BluetoothServices.h
@@ -0,0 +1,12 @@
+//
+//  BluetoothServices.h
+//  Broadcaster
+//
+//  Created by Lukasz Kostka on 8/1/13.
+//  Copyright (c) 2013 Estimote, Inc. All rights reserved.
+//
+
+// Run Terminal app, run uuidgen (twice) to get your very own UUIDs
+
+#define SERVICE_UUID @"00000000-0000-0000-0000-000000000000"
+#define CHARACTERISTIC_UUID @"00000000-0000-0000-0000-000000000000"
---
+++ Assassination/Register.h
@@ -0,0 +1,13 @@
+//
+//  Register.h
+//  Assassination
+//
+//  Created by Hikari Senju on 4/21/14.
+//  Copyright (c) 2014 Hikari Senju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Register : UIViewController
+
+@end
